e55f49e8063ab81d1745208c4619577cd3188ba4
Daeduol <daniel.030211@gmail.com>
2023-12-07 00:37:34 -0600

    resolucion de bug en idioma y relacion de aspecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
             defaultLocale: "en",
             locales: ["en","es"],
             routing: {
-                prefixDefaultLocale: true
+                prefixDefaultLocale: false
             }
             
         }
