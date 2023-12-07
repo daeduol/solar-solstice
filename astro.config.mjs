326f480f1271c4407d0254d9222539f5e432f7b1
daduol <daniel.030211@gmail.com>
2023-12-06 18:05:35 -0600

    retiro de i18next para usar implemetacion oficial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +11,8 @@ export default defineConfig({
             defaultLocale: "en",
             locales: ["en","es"],
             routing: {
-                prefixDefaultLocale: false
+                prefixDefaultLocale: true
             }
+            
         }
 });
