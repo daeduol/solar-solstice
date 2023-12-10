439628bff9c916ee08b6b301402018b1aa158b2d
daniel <daniel.030211@gmail.com>
2023-12-10 08:17:45 -0600

    traduccion de componentes proyecto, contato y ajustes en accesibilidad para sus imagenes

--- src/content/projects/project-3.md
+++ src/content/projects/project-3.md
@@ -2,7 +2,7 @@
 inProgress: false
 title: Project title
 description: Project description
-img_alt: project image alt text
+img_alt: Imagen minimalista en tonos pastel que representa proyectos de backend en Java. Una laptop muestra código, manos escriben, y un globo con '</>' destaca sobre la pantalla. En el fondo, engranes simbolizan trabajo e ingeniería.
 link: https://github.com/veranikabarel?tab=repositories
 tags: ['React', 'CSS', 'Typescript']
 ---
--- src/i18n/ui/ui.ts
+++ src/i18n/ui/ui.ts
@@ -12,6 +12,8 @@ export const languages = {
       'proyects': 'Proyects',
       'contact': 'Contact',
       'learn more': 'Learn more',
+      'say hello' : 'Say hello!',
+      'view full' : 'View full private projects archive',
     },
     es: {
       'cv button': 'Descargar CV',
@@ -19,5 +21,7 @@ export const languages = {
       'proyects': 'Proyectos',
       'contact': 'Contacto',
       'learn more': 'Saber más',
+      'say hello' : 'Saluda!',
+      'view full' : 'mira el archivo de proyectos privado completo' 
     },
   } as const;
