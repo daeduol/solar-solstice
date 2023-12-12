796149a28afb9b82c825c75b5ffc347e7e29e7bf
daniel <daniel.030211@gmail.com>
2023-12-11 18:51:22 -0600

    elimarcion de modulos i18next e instalacion de modulo node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
-
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx()],
-        i18n: {
-            defaultLocale: "en",
-            locales: ["en","es"],
-            routing: {
-                prefixDefaultLocale: false
-            }
-            
-        }
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [tailwind(), mdx()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
--- src/i18n/ui/ui.ts
+++ src/i18n/ui/ui.ts
@@ -3,7 +3,7 @@ export const languages = {
     es: 'Español',
   };
   
-  export const defaultLang = 'en';
+  export const defaultLang = 'es';
   
   export const ui = {
     en: {
