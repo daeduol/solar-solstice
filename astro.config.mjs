0b19c23c73e8e88a206121664930c23ddf39fc18
Daniel Olano <daniel.030211@gmail.com>
2024-10-20 22:13:01 -0600

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: deno(),
+	site: "https://daeduol@github.io",
+	base: '/solar-solstice/',
 	integrations: [tailwind(), mdx()],
 	i18n: {
 		defaultLocale: 'en',
